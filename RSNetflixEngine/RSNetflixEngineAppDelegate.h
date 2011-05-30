cf9696b19a5b21da95b559552d9869c9ba299b61
Rizwan Sattar <rsattar@gmail.com>
2011-05-29 19:19:58 -0700

    Initial Commit

---
+++ RSNetflixEngine/RSNetflixEngineAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  RSNetflixEngineAppDelegate.h
+//  RSNetflixEngine
+//
+//  Created by Rizwan on 5/29/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RSNetflixEngineAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ RSNetflixEngine/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  RSNetflixEngine
+//
+//  Created by Rizwan on 5/29/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
